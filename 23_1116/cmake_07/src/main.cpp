cfffdf5122f966b6eda1696cc56900ca0bdf13b0
youWei2 <2237453362@qq.com>
2023-11-16 11:10:37 +0800

    [231116] cmake 链接动态库或者静态库

---
+++ 23_1116/cmake_07/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "func1.h"
+
+int main()
+{
+    printf_func1((char*)"func1: has link the func");
+    return 0;
+}
---
+++ 23_1116/cmake_07/testFunc/include/func1.h
@@ -0,0 +1,5 @@
+#ifndef FUNC1_H
+#define FUNC1_H
+#include <iostream>
+int printf_func1(char* str);
+#endif
